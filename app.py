06669f354ae70f9bdd322095e20d41faa80931b3
abhayas <sahuabhayaa333@gmail.com>
2025-12-14 10:19:00 +0530

    Initial API setup

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+import numpy as np
+import sklearn
+import os
+from dotenv import load_dotenv
+import psycopg2
+import json
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+@app.route("/")
+def home():
+    return{"status": "ok"}
+
+@app.route("/health")
+def health():
+    return '<h1> Hello how are you? </h1>'
+
+def get_conn():
+    return psycopg2.connect(DATABASE_URL,sslmode="require")
+
+
+
+@app.route("/predict")
+def predict():
+    loadmodel = joblib.load("model/kyphosis-model_new.pkl")
+    xmanual = np.array([[158,3,14]])
+    predimanual = loadmodel.predict(xmanual)
+    conn = get_conn()
+    cur = conn.cursor()
+    payload = json.dumps(predimanual[0].tolist())
+   
+
+    cur.execute(
+
+        """
+        INSERT INTO  visits ( page_path, model_name, prediction, probability, payload)
+        VALUES (%s, %s, %s, %s, %s)""",
+
+        ("/predict", "kyphosis-model_new","jsonify(predimanual[0].tolist())", 1,payload )
+    )
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+
+    return jsonify(predimanual[0].tolist())
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
